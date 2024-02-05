c575b608ba79266691cc4a3a5cb97da4a90350c3
Noman Shaikh <noman.shaikh@neosftmail.com>
2024-02-05 16:26:59 +0530

    Client controlllers

---
+++ backend/controllers/client.controller.js
@@ -0,0 +1,81 @@
+import Client from "../models/client.model.js";
+import User from "../models/user.model.js";
+
+const createClient = async (req, res, next) => {
+  //   console.log("req.body", req.body);
+
+  const { name, email, GST_No, address } = req.body;
+  try {
+    const createNewClient = await Client.create({
+      user: req.user._id,
+      name,
+      email,
+      address,
+      GST_No,
+    });
+    res.json(createNewClient);
+  } catch (error) {
+    console.log("error", error);
+    next(error);
+  }
+};
+
+const getAllClients = async (req, res, next) => {
+  try {
+    const getClients = await Client.find({}).populate({
+      path: "user",
+      model: User,
+      select: "name email _id isAdmin",
+    });
+    return res.json(getClients);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getClientById = async (req, res, next) => {
+  const { id } = req.params;
+  //   console.log("id", id);
+  try {
+    const client = await Client.findById(id).populate({
+      path: "user",
+      model: User,
+      select: "name email _id isAdmin",
+    });
+    if (client) {
+      return res.json(client);
+    }
+    throw new Error("Client not found");
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateClientById = async (req, res, next) => {
+  const { id } = req.params;
+  const { name, email, GST_No, address } = req.body;
+
+  try {
+    const client = await Client.findById(id);
+    if (!client) {
+      throw new Error("Client not found");
+    }
+    const updateClient = await Client.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          name,
+          email,
+          address,
+          GST_No,
+        },
+      },
+      { new: true }
+    );
+    return res.json(updateClient);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createClient, getAllClients, getClientById, updateClientById };
--- backend/routes/client.route.js
+++ backend/routes/client.route.js
@@ -1,4 +1,14 @@
 import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+import {
+  createClient,
+  getAllClients,
+  getClientById,
+  updateClientById,
+} from "../controllers/client.controller.js";
 const router = express.Router();
+
+router.route("/").post(protect, createClient).get(protect, getAllClients);
+router.route("/:id").get(protect, getClientById).put(protect, updateClientById);
 
 export default router;
