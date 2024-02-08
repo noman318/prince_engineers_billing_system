00a7419c83131960f560cd4b9fe3eb14c87f7107
Noman Shaikh <noman.shaikh@neosftmail.com>
2024-02-08 09:53:44 +0530

    Minor UI Fixes

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const FormContainer = ({ children }) => {
-  return <Container>{children}</Container>;
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6}>
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default FormContainer;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,55 @@
 import React from "react";
-import { Nav, NavDropdown, Navbar, Container } from "react-bootstrap";
-
+import {
+  Nav,
+  // eslint-disable-next-line no-unused-vars
+  NavDropdown,
+  Navbar,
+  Container,
+  Form,
+  Button,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Prince Engg.</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
+      <Container>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand href="#">Prince Engg.</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action2">Link</Nav.Link>
+              </Nav>
+              <Nav className="ml-auto my-2 my-lg-0">
+                <Link to={"/login"}>
+                  <Nav.Link href="#action2">Login</Nav.Link>
+                </Link>
+                <Nav.Link href="#action2">Register</Nav.Link>
+                {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+                
+                <NavDropdown.Item href="#action4">Admin </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+                <NavDropdown.Item href="#action5">Register </NavDropdown.Item>
+              </NavDropdown> */}
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
     </header>
   );
 };
