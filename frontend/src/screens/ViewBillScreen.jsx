6072b37c937fae727352211a17efb3887cc479ac
Noman Shaikh <noman.shaikh@neosftmail.com>
2024-02-16 11:42:34 +0530

    Download Bill PDF done

--- frontend/src/screens/ViewBillScreen.jsx
+++ frontend/src/screens/ViewBillScreen.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetBillByIdQuery } from "../slices/billsApiSlice";
 import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import ViewBillHeader from "../components/ViewBill/ViewBillHeader";
 import BillHeader from "../components/ViewBill/BillHeader";
 import AmountData from "../components/ViewBill/AmountData";
 import ViewBillFooter from "../components/ViewBill/ViewBillFooter";
 import "../components/ViewBill/ViewBillHeader.css";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 const ViewBillScreen = () => {
   const { id } = useParams();
 
   const { data, isLoading, isError } = useGetBillByIdQuery(id);
+  const [loading, setLoading] = useState(false);
   // console.log("data", data);
+  const downloadPdf = async () => {
+    try {
+      setLoading(true);
+      const capture = document.querySelector(".bill-pdf");
+      console.log("capture", capture);
+      setLoading(true);
+      html2canvas(capture).then((canvas) => {
+        const imgData = canvas.toDataURL("img/png");
+        const doc = new jsPDF("p", "mm", "a4");
+        const componentWidth = doc.internal.pageSize.getWidth();
+        const componentHeight = doc.internal.pageSize.getHeight();
+        doc.addImage(imgData, "PNG", 0, 0, componentWidth, componentHeight);
+        setLoading(false);
+        doc.save(`${data?.invoice_no}.pdf`);
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <Container className="mt-4">
-      <h4>Bill No. {data?.invoice_no}</h4>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h4>Bill No. {data?.invoice_no}</h4>
+        <Button variant="secondary mb-3" onClick={downloadPdf}>
+          {loading ? "Downloading Bill" : "Download Bill"}
+        </Button>
+      </div>
       {isLoading ? (
         <div>
           <Loader />
@@ -24,7 +57,7 @@ const ViewBillScreen = () => {
       ) : isError ? (
         <div>Something went wrong</div>
       ) : (
-        <>
+        <div className="bill-pdf">
           <Container className="border border-2 p-4">
             <p className="text-center">Tax Invoice</p>
             <hr />
@@ -50,7 +83,7 @@ const ViewBillScreen = () => {
             <hr />
             <ViewBillFooter />
           </Container>
-        </>
+        </div>
       )}
     </Container>
   );
