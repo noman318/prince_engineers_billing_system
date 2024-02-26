966358a984a493c8e20563d57a128e3e45bf9137
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2024-02-26 23:32:04 +0530

    Ready to be deployed

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectToDb from "./config/db.js";
 import colors from "colors";
 import cookieParser from "cookie-parser";
@@ -16,14 +17,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.json("API is running");
-});
+// app.get("/", (req, res) => {
+//   res.json("API is running");
+// });
 
 app.use("/api/users", userRoutes);
 app.use("/api/clients", clientRoutes);
 app.use("/api/bills", billRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  // console.log("__dirname", __dirname);
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
